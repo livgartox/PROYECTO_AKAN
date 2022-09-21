6d9221cf8b681e0d1a1f1e4cebe7327c37fef3b3
ARTUROLOPEZROD <a.lr9710al@gmail.com>
2022-09-20 20:10:57 -0500

    actualizacion js formularios

--- assets/js/LogIn.js
+++ assets/js/LogIn.js
@@ -1,47 +1,32 @@
-//funcion que captura datos del formulario iniciar sesoon
+const formulario = document.getElementById("formInicioSesion");
+
+formulario.addEventListener("submit", (e) => {
+  let correo = document.getElementById("correo").value;
+  let contraseña = document.getElementById("password").value;
+  const expresionCorreo = /\w+@\w+\.+[a-z]/;
+  const expresionPass = /^.{4,12}$/;
+
+  if (!expresionCorreo.test(correo)) {
+    alert("El correo es invalido");
+    document.getElementById("correo").focus();
+  }
+  if (!expresionPass.test(contraseña)) {
+    alert(
+      "la contraseña es Invalida" +
+        "\nLa contraseña debe tener de 4 a 12 caracteres"
+    );
+    document.getElementById("password").focus();
+  } else {
+    console.log(correo + " " + contraseña);
+    document.getElementById("correo").value = "";
+    document.getElementById("password").value = "";
+  }
+
+  let sesion = { correo, contraseña };
+  let sesionJson = JSON.stringify(sesion);
+  console.log(sesion);
+  console.log(sesionJson);
 
 
-//funcion que captura datos del formulario iniciar sesoon
-function inicioSesion(){
-    let correo= document.getElementById('correo').value;
-    let contraseña= document.getElementById('password').value;
-    const expresionCorreo = /\w+@\w+\.+[a-z]/;
-    const expresionPass = /^.{4,12}$/;
-
- /*    if (correo==''){
-        alert('Ingrese correo');
-        document.getElementById("correo").focus();
-        return false;
-    }
-     if (contraseña==''){
-        alert('Ingrese contraseña');
-        document.getElementById("contraseña").focus();
-        return false;
-    } */
-     if (!expresionCorreo.test(correo)){
-        alert('El correo es invalido')
-        document.getElementById("correo").focus();
-        return false;
-        
-    }
-     if (!expresionPass.test(contraseña)){
-        alert('la contraseña es Invalida'+'\nLa contraseña debe tener de 4 a 12 caracteres')
-        document.getElementById("password").focus();
-        return false;
-
-    } else{
-        console.log(correo+" "+contraseña);
-        document.getElementById('correo').value="";
-        document.getElementById('password').value="";
-
-    }
-
-
-    let sesion = {correo, contraseña}
-    let sesionJson = JSON.stringify(sesion);
-    console.log(sesion);
-    console.log(sesionJson);
-    return false;
-               
-}
-
+  e.preventDefault();
+});
--- assets/js/contraseñas.js
+++ assets/js/contraseñas.js
@@ -1,51 +1,41 @@
 
- function contraseOlv(){
-    let correoElec2=document.getElementById('correo2').value;
-    let contraseña1=document.getElementById('contra-1').value;
-    let contraseña2=document.getElementById('contra-2').value;
-    const expresionCorreo = /\w+@\w+\.+[a-z]/;
-    const expresionPass = /^.{4,12}$/;
 
-    /* if (correoElec2==""){
-        //alert("Ingrese correo Electronico");
-        document.getElementById("correo2").focus();
-        return false;
-    }else
-        if (contraseña1==""){
-            //alert("Ingrese Contraseña");
-            document.getElementById("contra-1").focus();
-            return false
-    }else if (contraseña2===""){
-            //alert("Ingrese Contraseña2");
-            document.getElementById("contra-2").focus();
-            return false
+const formulario = document.getElementById("formContraseña");
 
-    } */ if (!expresionCorreo.test(correoElec2)){
-        alert('El correo es invalido')
-        document.getElementById("correo2").focus();
-        return false
-        
-    }
-     if (!expresionPass.test(contraseña1)){
-        alert('la contraseña es Invalida'+'\nLa contraseña debe tener de 4 a 12 caracteres')
-        document.getElementById("contra-1").focus();
-        return false
+formulario.addEventListener("submit", (e) => {
+  let correoElec2 = document.getElementById("correo2").value;
+  let contraseña1 = document.getElementById("contra-1").value;
+  let contraseña2 = document.getElementById("contra-2").value;
+  const expresionCorreo = /\w+@\w+\.+[a-z]/;
+  const expresionPass = /^.{4,12}$/;
 
-    } if (!expresionPass.test(contraseña2)){
-        alert('la contraseña es Invalida'+'\nLa contraseña debe tener de 4 a 12 caracteres')
-        document.getElementById("contra-2").focus();
-        return false
+
+
+    if (!expresionCorreo.test(correoElec2)) {
+    alert('El correo es invalido')
+    document.getElementById("correo2").focus();
+    //return false
+  }
+  if (!expresionPass.test(contraseña1)) {
+    alert('la contraseña es Invalida'+'\nLa contraseña debe tener de 4 a 12 caracteres')
+    document.getElementById("contra-1").focus();
+    //return false
+  }
+  if (!expresionPass.test(contraseña2)) {
+    alert('la contraseña es Invalida'+'\nLa contraseña debe tener de 4 a 12 caracteres')
+    document.getElementById("contra-2").focus();
+    //return false
     
-    } else {
-        console.log(correoElec2 +" "+contraseña1+" "+contraseña2);
-        document.getElementById("correo2").value="";
-        document.getElementById("contra-1").value="";
-        document.getElementById("contra-2").value="";
-        }
-        let recupContraseña = {correoElec2, contraseña1, contraseña2}
-        let recupContraseñaJson = JSON.stringify(recupContraseña );
-        console.log(recupContraseña );
-        console.log(recupContraseñaJson);
-         
-    }
- 
+  } else {
+    console.log(correoElec2 + " " + contraseña1 + " " + contraseña2);
+    document.getElementById("correo2").value = "";
+    document.getElementById("contra-1").value = "";
+    document.getElementById("contra-2").value = "";
+  }
+  let recupContraseña = { correoElec2, contraseña1, contraseña2 };
+  let recupContraseñaJson = JSON.stringify(recupContraseña);
+  console.log(recupContraseña);
+  console.log(recupContraseñaJson);
+
+  e.preventDefault();
+});
